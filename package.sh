8af529100c68af7fd0401dcb9700e86ff33219c1
PJ Eby <pje@telecommunity.com>
2023-04-14 21:40:45 -0400

    Update dependencies in build

--- package.sh
+++ package.sh
@@ -1,2 +1,2 @@
 BINS=bin/doco
-BUILD_DEPS=bashup/mdsh:bashup/jqmd:bashup/loco:bashup/realpaths:bashup/c3-mro
+BUILD_DEPS=bashup/mdsh:bashup/jqmd:bashup/loco:bashup/realpaths:bashup/c3-mro:bashup/events@bash44
